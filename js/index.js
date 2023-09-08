35ae5c2562f1d8e17d085310570a99f9fa13b27a
Nikmet <metlov.n@mail.ru>
2023-09-09 00:00:47 +0300

    dark theme css

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ const btnDArkMode = document.querySelector(".dark-mode-btn");
 
 btnDArkMode.addEventListener("click", () => {
     btnDArkMode.classList.toggle("dark-mode-btn--active");
+    document.body.classList.toggle('dark');
 });
